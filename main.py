585b391afe50221ee03bb8dfc6224ee7c90edbdc
Maxim <maxnes500@gmail.com>
2021-02-14 16:53:56 +0900

    ...

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
     comment_author = relationship("User", back_populates="comments")
 
-# create the database t
+# create the database tu
 db.create_all()
 
 
